4ad3345a23547215823b9062aa7fe4ef956998b0
立野韻 <hibik17@tatenoinnoMacBook-Pro.local>
2024-03-25 00:09:12 +0900

    tagを挿入して表示する処理をjqueryで作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,27 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"; 
+import 'bootstrap-tagsinput';
 
+$(document).ready(function() {
+  $("input[data-role='tagsinput']").tagsinput();
+});
+
+$(document).ready(function() {
+  // タグリストのテキストフィールドの値が変更されたときのイベントハンドラー
+  $('input[data-role="tagsinput"]').on('change', function() {
+    var tags = $(this).val().split(','); // カンマで区切られたタグを配列に分割
+    $('#tags-container').empty(); // 既存のタグをクリア
+
+    // 各タグに対してループ処理
+    $.each(tags, function(index, tag) {
+      // タグ用の新しいspan要素を作成し、テキストとスタイルを設定
+      var span = $('<span>').text(tag).addClass('tag-style');
+      // span要素をタグコンテナに追加
+      $('#tags-container').append(span);
+    });
+  });
+});
 
 Rails.start()
 Turbolinks.start()
