94674ecd9428ae6fea11c7c3263c4a0acecab84d
立野韻 <hibik17@tatenoinnoMacBook-Pro.local>
2024-03-25 00:08:45 +0900

    act as tagをsetup

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,9 @@
 class Book < ApplicationRecord
- 
   belongs_to :user
   has_one_attached :profile_image
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
+
+  acts_as_taggable_on :tags
   
 end
